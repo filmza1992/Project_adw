746c7b862d90bc4a4f86449f2bff1fd38186f00b
filmza1992 <ddza1992@hotmail.com>
2024-02-25 21:04:40 +0700

    Set up project

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,15 @@
+import { Box, Container } from "@mui/system";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Container>
+        <Box>
+          <h1 className="text-something text-xl3 font-bold underline">This is Login page!!!!</h1>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default LoginPage;
